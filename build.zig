c618331544445deafcd6046081148d20b79d5b02
Sam Atman <atmanistan@gmail.com>
2025-11-19 10:54:21 -0500

    Zitron-friendly build
    This took a minute to figure out!

--- build.zig
+++ build.zig
@@ -10,29 +10,30 @@ pub fn build(b: *std.Build) void {
         .target = b.graph.host,
         .optimize = .Debug,
         .enum_file = true,
+        .quiet = true,
     });
 
     const zitron_exe = zitron_dep.artifact("zitron");
 
     const zitron_run = b.addRunArtifact(zitron_exe);
 
-    const zitron_writedir = b.addWriteFiles();
+    const zitron_write_in = b.addWriteFiles();
 
-    const calc_dir = zitron_writedir.addCopyDirectory(b.path("src"), "calc", .{
-        .include_extensions = &.{ "zig", "zy" },
+    const calc_input_dir = zitron_write_in.addCopyDirectory(b.path("src"), "calc_in", .{});
+
+    zitron_run.setCwd(calc_input_dir);
+    zitron_run.addArg("calc.zy");
+    zitron_run.step.dependOn(&zitron_write_in.step);
+
+    const zitron_write_out = b.addWriteFiles();
+    zitron_write_out.step.dependOn(&zitron_run.step);
+
+    const calc_rootdir = zitron_write_out.addCopyDirectory(calc_input_dir, "", .{
+        .exclude_extensions = &.{"zy"},
     });
 
-    zitron_run.setCwd(calc_dir);
-    zitron_run.addArg("--file");
-    const calc_root = zitron_run.addOutputFileArg("calc.zig");
-    zitron_run.addArg("calc.zy");
-    zitron_run.step.dependOn(&zitron_writedir.step);
-
-    const gen_step = b.step("generate", "Generate grammar files");
-    gen_step.dependOn(&zitron_run.step);
-
     const calc_mod = b.addModule("calc_parser", .{
-        .root_source_file = calc_root,
+        .root_source_file = calc_rootdir.path(b, "calc.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -51,8 +52,6 @@ pub fn build(b: *std.Build) void {
     const run_exe_unit_tests = b.addRunArtifact(calc_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-
-    test_step.dependOn(gen_step);
 
     test_step.dependOn(&run_exe_unit_tests.step);
 }
